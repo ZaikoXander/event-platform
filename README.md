93e72fb61d69e39c18bc8097ad3ccf26d83af91a
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-06-21 13:03:04 -0300

    event-platform

---
+++ README.md
@@ -0,0 +1,14 @@
+CMS = Content Management System
+
+WordPress Traz tanto o painel de ADMIN tanto quanto a parte visual do front-end (temas)
+
+Headless CMS (GraphCMS): Painel de ADMIN (dados fornecidos através de uma API REST ou GraphQL)
+React que consome essa API do CMS
+
+Operações no GraphQL: query / mutation
+
+query = buscar dados
+mutation = criar, alterar ou deletar dados
+
+Overfating = backend devolvendo muito mais informação do que é necessário
+Underfating = backend devolvendo quantidade de informação não suficiente
---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+
+      teacher {
+        name
+      }
+    }
+  }
+`
+
+interface Lesson {
+  id: string
+  title: string
+}
+
+function App() {
+  /* useEffect(() => {
+    client.query({
+      query: GET_LESSONS_QUERY
+    }).then(response => {
+      console.log(response.data)
+    })
+  }, []) */
+
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+
+  console.log(data)
+
+  return (
+    <ul>
+      {data?.lessons.map(lesson => {
+        return (
+          <>
+            <li key={lesson.id}>{lesson.title}</li>
+          </>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default App
